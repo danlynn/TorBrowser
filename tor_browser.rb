c400bde7fb9734f68aecb8ea8713baee83fc28bb
Dan Lynn <dan@danlynn.org>
2017-09-28 15:40:20 -0400

    Updated to work with MacOS High Sierra + add ctrl-c support

--- tor_browser.rb
+++ tor_browser.rb
@@ -10,12 +10,13 @@ end
 
 def proxy_already_running?
   netstat_response = `netstat -an | grep 9150`
+  # puts "=== netstat_response: #{netstat_response}"
   if netstat_response == ''
-    sleep 5
     return false
   end
   curl_response = `curl -IkL --connect-timeout 5 -X HEAD -x socks://localhost:9150 https://www.google.com 2>/dev/null`
-  curl_response.lines[0] =~ /(?:200 OK)|(?:302 Found)/i
+  # puts "=== curl_response: #{curl_response}"
+  curl_response.lines[0] =~ /(?:\b200\b)|(?:\b302\b)/i
 end
 
 
@@ -43,12 +44,25 @@ def launch_browser_when_proxy_ready
         exit 0
       else
         log "--- waiting for tor to make connection (#{10-i} more tries)"
+        sleep 5
       end
     end
     log '--- giving up on tor connection - skipping launch of chrome browser'
     exit 0
   end
 end
+
+
+Signal.trap("INT") { # trap ctrl-c
+  container_id = `docker ps -q -f name=tor_instance`
+  if container_id
+    log '=== stopping tor due to ctrl-c'
+    `docker stop tor_instance`
+    log "\n=== tor successfully stopped\n"
+  end
+  exit 0
+  # TODO: kill off chrome by terminating Chrome instance that is child proc of this script
+}
 
 
 if proxy_already_running?
@@ -72,6 +86,10 @@ end
 
 
 # TODO: maybe run docker container as non-root user?
-# see: https://forums.docker.com/t/swiching-between-root-and-non-root-users-from-interactive-console/2269/2
-# also see how sabnzbd Dockerfile handled it: https://hub.docker.com/r/sabnzbd/sabnzbd/
+#   see: https://forums.docker.com/t/swiching-between-root-and-non-root-users-from-interactive-console/2269/2
+#   also see how sabnzbd Dockerfile handled it: https://hub.docker.com/r/sabnzbd/sabnzbd/
 
+# TODO: use --ash-host-window-bounds chrome arg to set window position/size for demo video
+#   see: http://peter.sh/experiments/chromium-command-line-switches/
+
+# TODO: use latest tag for tor docker image and pull latest image before launch
